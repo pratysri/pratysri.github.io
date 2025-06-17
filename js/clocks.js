f1c5470d923f090c945d41316f35293048553b77
Praty <pratsetu@gmail.com>
2025-06-17 00:01:32 -0400

    fized more stuff

--- js/clocks.js
+++ js/clocks.js
@@ -1,31 +1,87 @@
-function updateClocks() {
-    const now = new Date();
-    
-    const digitalClock = document.getElementById('digital-clock');
-    if (digitalClock) {
-        digitalClock.textContent = now.toLocaleTimeString();
+function displayTime() {
+    document.getElementById("digital-clock").innerHTML = new Date().toLocaleTimeString();
+}
+setInterval(displayTime, 1000);
+
+function initAnalogClock() {
+    var canvas = document.getElementById("analog-clock");
+    var ctx = canvas.getContext("2d");
+    var radius = canvas.height / 2;
+    ctx.translate(radius, radius);
+    radius *= 0.90;
+    setInterval(drawClock, 1000);
+
+    function drawClock() {
+        drawFace(ctx, radius);
+        drawNumbers(ctx, radius);
+        drawTime(ctx, radius);
     }
-    
-    const analogClock = document.getElementById('analog-clock');
-    if (analogClock) {
-        const hours = now.getHours() % 12;
-        const minutes = now.getMinutes();
-        const seconds = now.getSeconds();
-        
-        const hourDeg = (hours * 30) + (minutes * 0.5);
-        const minuteDeg = minutes * 6;
-        const secondDeg = seconds * 6;
-        
-        analogClock.innerHTML = `
-            <div class="clock-face">
-                <div class="hand hour-hand" style="transform: rotate(${hourDeg}deg)"></div>
-                <div class="hand minute-hand" style="transform: rotate(${minuteDeg}deg)"></div>
-                <div class="hand second-hand" style="transform: rotate(${secondDeg}deg)"></div>
-                <div class="center-dot"></div>
-            </div>
-        `;
+
+    function drawFace(ctx, radius) {
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'white';
+        ctx.fill();
+        var grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+        grad.addColorStop(0, '#333');
+        grad.addColorStop(0.5, 'white');
+        grad.addColorStop(1, '#333');
+        ctx.strokeStyle = grad;
+        ctx.lineWidth = radius * 0.1;
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+        ctx.fillStyle = '#333';
+        ctx.fill();
+    }
+
+    function drawNumbers(ctx, radius) {
+        var ang;
+        var num;
+        ctx.font = radius * 0.15 + "px arial";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        for (num = 1; num <= 12; num++) {
+            ang = num * Math.PI / 6;
+            ctx.rotate(ang);
+            ctx.translate(0, -radius * 0.85);
+            ctx.rotate(-ang);
+            ctx.fillText(num.toString(), 0, 0);
+            ctx.rotate(ang);
+            ctx.translate(0, radius * 0.85);
+            ctx.rotate(-ang);
+        }
+    }
+
+    function drawTime(ctx, radius) {
+        var now = new Date();
+        var hour = now.getHours();
+        var minute = now.getMinutes();
+        var second = now.getSeconds();
+
+        hour %= 12;
+        hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (360 * 60));
+        drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+
+        minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+        drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+
+        second = (second * Math.PI / 30);
+        drawHand(ctx, second, radius * 0.9, radius * 0.02);
+    }
+
+    function drawHand(ctx, pos, length, width) {
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.lineCap = "round";
+        ctx.moveTo(0, 0);
+        ctx.rotate(pos);
+        ctx.lineTo(0, -length);
+        ctx.stroke();
+        ctx.rotate(-pos);
     }
 }
 
-setInterval(updateClocks, 1000);
-updateClocks(); 
+document.addEventListener('DOMContentLoaded', function() {
+    initAnalogClock();
+}); 
